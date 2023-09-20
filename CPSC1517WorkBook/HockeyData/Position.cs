00712a49f7ef404110d848daa42052af68274ec0
Jacqueline Tanguay <jtanguay5@nait.ca>
2023-09-20 13:02:25 -0600

    Hockey Player Position and Shot Enums

---
+++ CPSC1517WorkBook/HockeyData/Position.cs
@@ -0,0 +1,14 @@
+﻿namespace Hockey.Data
+{
+    /// <summary>
+    /// Represents the available hockey player positions
+    /// </summary>
+    public enum Position
+    {
+        LeftWing,
+        RightWing,
+        Center,
+        Defense,
+        Goalie
+    }
+}
---
+++ CPSC1517WorkBook/HockeyData/Shot.cs
@@ -0,0 +1,11 @@
+﻿namespace Hockey.Data
+{
+    /// <summary>
+    /// Represents the shot hand for the hockey player.
+    /// </summary>
+    public enum Shot
+    {
+        Left,
+        Right
+    }
+}
